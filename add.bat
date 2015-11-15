e71dcea285591cffbce6ed75bf83530637da569c
sdmaxim <maxim.sidich@gmail.com>
2015-11-15 10:57:17 +0200

    15.11.15

--- add.bat
+++ add.bat
@@ -1,10 +1,11 @@
-git add db
-git add node_modules
-git add public
-git add routes
-git add views
-git add push.bat
-git add pull.bat
-git add git.txt
-git add app.js
-git add add.bat
+git add -A db
+git add -A node_modules
+git add -A public
+git add -A routes
+git add -A views
+git add -A .idea
+git add -A push.bat
+git add -A pull.bat
+git add -A git.txt
+git add -A app.js
+git add -A add.bat
